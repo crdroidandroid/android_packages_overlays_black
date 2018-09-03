2d7f2291b6369a88145227946771b26ba2b9da68
Pranav Vashi <neobuddy89@gmail.com>
2018-09-04 02:21:21 +0530

    black: shut up.. God's watching
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- system/Android.mk
+++ system/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_RRO_THEME := SystemBlackTheme
+LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := SystemBlackTheme
